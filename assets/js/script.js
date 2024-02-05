a2bcef8adb2c3cf2f4a417bcc0803e40b6c9368c
GiorgioDaVinci <okoriegeorge1@gmail.com>
2024-02-05 13:27:52 +0000

    I am having issues merging my files

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ const weatherAPI = "aefc3d833526ac11a10cd57951b4969d";
 // The variable keeps track of the current page
 let currentPage = 1;
 
-
 function displayEvents(events) {
   // Created an events container in the form of an id to store all the event details
   const eventsContainer = $("#events");
@@ -13,13 +12,15 @@ function displayEvents(events) {
   // The if statement checks to see that the array, events is not empty before proceeding with the loop
   if (events.length > 0) {
     events.forEach((event) => {
-      // I created a variable that stores the event details 
-      const eventName = event.name || 'Event Name Not Available'; 
-      const eventStartDate = event.dates.start.localDate || 'Start Date Not Available';
-      const venueName = event._embedded.venues[0].name || 'Venue Name Not Available';
-      const venueTime = event.dates.start.localTime
-      const buttonLink = event.url
-      const eventImage = event.images[0].url
+      // I created a variable that stores the event details
+      const eventName = event.name || "Event Name Not Available";
+      const eventStartDate =
+        event.dates.start.localDate || "Start Date Not Available";
+      const venueName =
+        event._embedded.venues[0].name || "Venue Name Not Available";
+      const venueTime = event.dates.start.localTime;
+      const buttonLink = event.url;
+      const eventImage = event.images[0].url;
 
       // The variable eventHtml stores the html tags where the event details are embedded
       const eventHtml = `
@@ -36,33 +37,39 @@ function displayEvents(events) {
         </div>
       `;
       // Appends the details received from the container with id, events into the html tags
-      const buttonSection = $("<div>").attr("class", "row")
-      const eventButton = $("<button>").attr("class", "event-button btn btn-warning col-12 mx-auto mb-5").text("Buy Tickets Now!")
-      eventButton.on("click", function(){
-        window.open(buttonLink)
-      })
-      eventsContainer.append(eventHtml, buttonSection)
-      buttonSection.append(eventButton)
+      const buttonSection = $("<div>").attr("class", "row");
+      const eventButton = $("<button>")
+        .attr("class", "event-button btn btn-warning col-12 mx-auto mb-5")
+        .text("Buy Tickets Now!");
+      eventButton.on("click", function () {
+        window.open(buttonLink);
+      });
+      eventsContainer.append(eventHtml, buttonSection);
+      buttonSection.append(eventButton);
+    });
+    // I am adding a div that will store the buttons at the end of the page
+    const buttonDiv = $("<div>").attr("class", "row mt-3");
 
-      // I am adding a div that will store the buttons at the end of the page
-      const buttonDiv = $("<div>").attr("class", "row mt-3");
-      
-      // The variable stores the previous button and when it is clicked it returns to the previous page and displays the data
-      const prevBtn = $("<button>").attr("class", "btn btn-secondary col-6").text("Previous").on("click", function(){
-        if(currentPage>1){
+    // The variable stores the previous button and when it is clicked it returns to the previous page and displays the data
+    const prevBtn = $("<button>")
+      .attr("class", "btn btn-secondary col-6")
+      .text("Previous")
+      .on("click", function () {
+        if (currentPage > 1) {
           currentPage--;
         }
       });
 
-      // The variable below stores the next button and fetches new data when it is clicked
-      const nextBtn = $("<button>").attr("class", "btn btn-primary col-6").text("Next").on("click", function(){
+    // The variable below stores the next button and fetches new data when it is clicked
+    const nextBtn = $("<button>")
+      .attr("class", "btn btn-primary col-6")
+      .text("Next")
+      .on("click", function () {
         currentPage++;
       });
 
-      buttonDiv.append(prevBtn, nextBtn);
-      eventsContainer.append(buttonDiv)
-      
-    });
+    buttonDiv.append(prevBtn, nextBtn);
+    eventsContainer.append(buttonDiv);
   } else {
     eventsContainer.text("No events found for the selected location.");
   }
@@ -75,7 +82,11 @@ $(function () {
     event.preventDefault();
 
     const searchLocation = $("#search-input").val();
-    const geoURL = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchLocation + "&limit=1&appid=" + weatherAPI;
+    const geoURL =
+      "https://api.openweathermap.org/geo/1.0/direct?q=" +
+      searchLocation +
+      "&limit=1&appid=" +
+      weatherAPI;
 
     fetch(geoURL)
       .then(function (response) {
